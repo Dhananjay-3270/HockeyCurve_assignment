b27d35865e2b825ee8339a26bb9fe90f87385d8f
Dhananjay-3270 <shinde3270@gmail.com>
2024-08-14 14:44:59 +0530

    BASICUI1

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="task-list-view">
+        <header>
+          <div className="task-list-header">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="25"
+              fill="currentColor"
+              class="bi bi-folder-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z" />
+            </svg>
+            Task List View
+          </div>
+        </header>
+
+        <div className="task-list-actions">
+          <div className="addtask-btn-div">
+            <button className="addtask-btn"> + Add new task</button>
+           
+          </div>
+           <div className="task-list-filters">
+              <button className="filter-active">All</button>
+              <button className="filter">High</button>
+              <button className="filter">Medium</button>
+              <button className="filter">Low</button>
+              <button className="filter">Done</button>
+            </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
